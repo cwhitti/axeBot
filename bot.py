7760f8e719df2b96d7a4dcd776d3f499c111af86
cwhitti <twiggleicious@gmail.com>
2023-09-07 20:12:53 -0700

    hmm

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ def run_discord_bot(axeBot = axeBot):
         return 0
 
     if msg.content == f"{axeBot.prefix}hi": # Testrun
-        await msg.channel.send("Listening! 123")
+        await msg.channel.send("Listening! 111111")
         return 0
 
     if (msg.author.id == 343857226982883339) and (msg.content == f"{axeBot.prefix}end"):
