4db0e33290ecb43f9082a842d307812e226abad8
cwhitti <twiggleicious@gmail.com>
2023-09-07 20:30:41 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 import bot
 
+def restart_bot():
+  os.execv(sys.executable, ['python'] + sys.argv)
+
 if __name__ == '__main__':
   #run the bot
   bot.run_discord_bot()
 
   print("hi there")
   restart_bot()
-
-def restart_bot():
-  os.execv(sys.executable, ['python'] + sys.argv)
