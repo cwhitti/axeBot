623181756ef8cde72f6e6c46abb2c605c53578f1
cwhitti <twiggleicious@gmail.com>
2023-09-11 09:15:57 -0700

    Cosmetic update

--- bot.py
+++ bot.py
@@ -46,23 +46,34 @@ def run_discord_bot(axeBot = axeBot):
             return 0
 
         if (msg.author.id == axeBot.owner) and (msg.content == f"{axeBot.prefix}end"):
-            print(f"{client.user} is now stopped.")
+            embed = discord.Embed(title=f"",
+                description="Ending...",
+                color=axeBot.color)
+            await msg.channel.send(embed=embed)
             await client.close()
 
-        if (msg.author.id == axeBot.owner) and (msg.content == f"{axeBot.prefix}pull"):
+        if (msg.author.id == axeBot.owner) and (msg.content == f"{axeBot.prefix}update"):
             if update_bot():
-                embed = discord.Embed(title=f"", description="Bot updated...", color=axeBot.color)
+                embed = discord.Embed(title=f"",
+                    description="Bot updated...",
+                    color=axeBot.color)
                 await msg.channel.send(embed=embed)
                 time.sleep(1)
-                embed = discord.Embed(title=f"", description="Restarting...", color=axeBot.color)
+                embed = discord.Embed(title=f"",
+                    description="Restarting...",
+                    color=axeBot.color)
                 await msg.channel.send(embed=embed)
                 restart_bot()
             else:
-                embed = discord.Embed(title=f"", description="Bot unable to be updated", color=axeBot.color)
+                embed = discord.Embed(title=f"",
+                    description="Bot unable to be updated",
+                    color=axeBot.color)
                 await msg.channel.send(embed=embed)
 
         if (msg.author.id == axeBot.owner) and (msg.content == f"{axeBot.prefix}restart"):
-            embed = discord.Embed(title=f"", description="Restarting...", color=axeBot.color)
+            embed = discord.Embed(title=f"",
+                description="Restarting...",
+                color=axeBot.color)
             await msg.channel.send(embed=embed)
             restart_bot()
 
@@ -77,14 +88,12 @@ def run_discord_bot(axeBot = axeBot):
         if msg.content.startswith(f"{axeBot.prefix}random"): # Gen random class
             async with msg.channel.typing():
                 # Check if the user is on cooldown
-                if msg.author.id in user_cooldowns and time.time() - user_cooldowns[msg.author.id] < axeBot.wait_limit:  # 5 seconds cooldown
-                    embed = discord.Embed(title=f"Rate Limit!", description="Please wait another few seconds before using this command.", color=axeBot.color)
+                if check_cooldown(axeBot, msg, user_cooldowns):  # 5 seconds cooldown
+                    embed = discord.Embed(title=f"Rate Limit!",
+                        description="Please wait another few seconds before using this command.",
+                        color=axeBot.color)
                     await msg.channel.send(embed=embed)
-
                     return 0
-                else:
-                    # Update the user's last message time
-                    user_cooldowns[msg.author.id] = time.time()
 
                 class_dict = random_class()
                 course_id_list = list(class_dict.keys())
@@ -107,20 +116,21 @@ def run_discord_bot(axeBot = axeBot):
             async with msg.channel.typing():
 
                 # Check if the user is on cooldown
-                if msg.author.id in user_cooldowns and time.time() - user_cooldowns[msg.author.id] < axeBot.wait_limit:  # 5 seconds cooldown
-                    embed = discord.Embed(title=f"Rate Limit!", description="Please wait another few seconds before using this command.", color=axeBot.color)
+                if check_cooldown(axeBot, msg, user_cooldowns):  # 5 seconds cooldown
+                    embed = discord.Embed(title=f"Rate Limit!",
+                        description="Please wait another few seconds before using this command.",
+                        color=axeBot.color)
                     await msg.channel.send(embed=embed)
 
                     return 0
-                else:
-                    # Update the user's last message time
-                    user_cooldowns[msg.author.id] = time.time()
 
                 args = msg.content.split()
                 input_course_code = ""
 
                 if len(args) == 1:
-                    embed = discord.Embed(title=f"Sorry, didn't catch that!", description=f"You can look up a class by using {axeBot.prefix}lookup <XXX000>", color=axeBot.color)
+                    embed = discord.Embed(title=f"Sorry, didn't catch that!",
+                        description=f"You can look up a class by using {axeBot.prefix}lookup <XXX000>",
+                        color=axeBot.color)
                     await msg.channel.send(embed=embed)
                     return 0
 
@@ -132,6 +142,7 @@ def run_discord_bot(axeBot = axeBot):
 
                 # Embed the full class
                 if len(url_list) <= 5:
+
                     class_dict = get_class_dict(url_list)
 
                     if class_dict:
@@ -141,7 +152,9 @@ def run_discord_bot(axeBot = axeBot):
                             await msg.channel.send(embed=embed)
 
                     else:
-                        embed = discord.Embed(title=f"{input_course_code}", description="Sorry, we couldn't find this course.", color=axeBot.color)
+                        embed = discord.Embed(title=f"{input_course_code}",
+                            description="Sorry, we couldn't find this course.",
+                            color=axeBot.color)
                         await msg.channel.send(embed=embed)
 
                 # shows a list of names to look up
@@ -162,19 +175,27 @@ def run_discord_bot(axeBot = axeBot):
                             await msg.channel.send(embed=embed)
                             first_embed = False
                     else:
-                        embed = discord.Embed(title=f"{input_course_code}", description="Sorry, we couldn't find this course.", color=axeBot.color)
+                        embed = discord.Embed(title=f"{input_course_code}",
+                            description="Sorry, we couldn't find this course.",
+                            color=axeBot.color)
                         await msg.channel.send(embed=embed)
                 else:
-                    embed = discord.Embed(title=f"{input_course_code}", description="Sorry, we couldn't find this course.", color=axeBot.color)
+                    embed = discord.Embed(title=f"{input_course_code}",
+                        description="Sorry, we couldn't find this course.",
+                        color=axeBot.color)
                     await msg.channel.send(embed=embed)
 
         if msg.content.startswith(f"{axeBot.prefix}prereqs"):
-            embed = discord.Embed(title=f"Course Prereqs", description="Working on that!", color=axeBot.color)
+            embed = discord.Embed(title=f"Course Prereqs",
+                description="This feature is still in progress.",
+                color=axeBot.color)
             await msg.channel.send(embed=embed)
 
     except Exception as e:
         raise e
-        embed = discord.Embed(title="Error", description=f"{e}", color=axeBot.color)
+        embed = discord.Embed(title="Error",
+            description=f"{e}",
+            color=axeBot.color)
         await msg.channel.send(embed=embed)
 
   client.run(axeBot.token)
@@ -189,20 +210,42 @@ def initialize_bot(axeBot):
 
 def create_help_embed(axeBot):
 
-    embed = discord.Embed(title="Axe Bot Help", description="List of available commands:", color=axeBot.color)
-    embed.add_field(name=f"{axeBot.prefix}help", value="Show a list of commands", inline=False)
-    embed.add_field(name=f"{axeBot.prefix}sujects", value="Look up all class subjects at NAU", inline=False)
-    embed.add_field(name=f"{axeBot.prefix}lookup <XXX>", value="Look up all classes for a specific subject", inline=False)
-    embed.add_field(name=f"{axeBot.prefix}lookup <XXX000>", value="Look up a specific class", inline=False)
-    embed.add_field(name=f"{axeBot.prefix}random", value="Find a random class", inline=False)
-    embed.add_field(name=f"{axeBot.prefix}prereqs", value="Show course prerequisites (work in progress)", inline=False)
+    embed = discord.Embed(title="Axe Bot Help",
+        description="List of available commands:", color=axeBot.color)
+
+    embed.add_field(name=f"{axeBot.prefix}help",
+        value="Show a list of commands",
+        inline=False)
+
+    embed.add_field(name=f"{axeBot.prefix}sujects",
+        value="Look up all class subjects at NAU",
+        inline=False)
+
+    embed.add_field(name=f"{axeBot.prefix}lookup <XXX>",
+        value="Look up all classes for a specific subject",
+        inline=False)
+
+    embed.add_field(name=f"{axeBot.prefix}lookup <XXX000>",
+        value="Look up a specific class",
+        inline=False)
+
+    embed.add_field(name=f"{axeBot.prefix}random",
+        value="Find a random class",
+        inline=False)
+
+    embed.add_field(name=f"{axeBot.prefix}prereqs",
+        value="Show course prerequisites (work in progress)",
+        inline=False)
+
     embed.set_footer(text="(!) This bot is not affiliated, sponsored, nor endorsed by NAU.")
 
     return embed
 
-def create_subjects_embed(axeBot,name_list):
+def create_subjects_embed(axeBot, name_list):
 
-    embed = discord.Embed(title="NAU Subjects", description="List of available topics:", color=axeBot.color)
+    embed = discord.Embed(title="NAU Subjects",
+        description="List of available topics:",
+        color=axeBot.color)
 
     start_letter = 'A'
     name_string = ""
@@ -235,37 +278,59 @@ def one_embed_course(axeBot,course_id, course_data):
     course_semesters = course_data[5]
     course_url = create_course_url(f"course?courseId={course_id}&term=1237")
 
-    if len(course_description) > 1024:
-        cont_message = ". . ."
-        end_index = 1024 - len(cont_message)
-        course_description = course_description[0:end_index] + cont_message
-
     embed = discord.Embed(title=course_name, description="", color=axeBot.color)
-    embed.add_field(name="Course ID:", value=course_id, inline=False)
-    embed.add_field(name="Course Description:", value=course_description, inline=False)
-    embed.add_field(name="Course Units:", value=course_units, inline=False)
-    embed.add_field(name="Course Semesters:", value=course_semesters, inline=False)
-    embed.add_field(name="Course Designation:", value=course_designation, inline=False)
-    embed.add_field(name="Course Prerequisites:", value=course_prerequisites, inline=False)
-    embed.add_field(name="", value=f"[Course Link]({course_url})", inline=False)
+    embed.add_field(name="Course ID:",
+        value=course_id,
+        inline=False)
+    embed.add_field(name="Course Description:",
+        value=course_description,
+        inline=False)
+    embed.add_field(name="Course Units:",
+        value=course_units,
+        inline=False)
+    embed.add_field(name="Course Semesters:",
+        value=course_semesters,
+        inline=False)
+    embed.add_field(name="Course Designation:",
+        value=course_designation,
+        inline=False)
+    embed.add_field(name="Course Prerequisites:",
+        value=course_prerequisites,
+        inline=False)
+    embed.add_field(name="",
+        value=f"[Course Link]({course_url})",
+        inline=False)
 
     return embed
 
 def batch_embed_course(axeBot,batch_keys, class_dict, first_embed):
 
     if first_embed == True:
-        embed = discord.Embed(title=f"{len(class_dict)} RESULTS FOUND", color=axeBot.color)
+        embed = discord.Embed(title=f"{len(class_dict)} RESULTS FOUND",
+            color=axeBot.color)
 
     else:
-        embed = discord.Embed(title="", color=axeBot.color)
+        embed = discord.Embed(title="",
+            color=axeBot.color)
 
     for course_id in batch_keys:
         course_name = class_dict[course_id]
-        embed.add_field(name=course_name, value=f"CourseID: {course_id}", inline=False)
+        embed.add_field(name=course_name,
+            value=f"CourseID: {course_id}",
+            inline=False)
     return embed
+
+def check_cooldown(axeBot, msg, user_cooldowns):
+
+    if (msg.author.id in user_cooldowns) and (time.time() - user_cooldowns[msg.author.id] < axeBot.wait_limit):
+        return True
+
+    user_cooldowns[msg.author.id] = time.time()
+    return False
 
 def update_bot():
 
+    # TO DO: check if branch is up to date
     try:
         subprocess.check_output(['git', 'pull'], stderr=subprocess.STDOUT, cwd='/root/bots/axeBot')
         return True
@@ -276,6 +341,8 @@ def update_bot():
         return False
 
 def restart_bot():
+
+    # TO DO: Find pwd automatically
     try:
         os.execv("/usr/bin/python3", ["python"] + ["/root/bots/axeBot/run.py"])
     except Exception as e:
