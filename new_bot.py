8d38f33e362391504440071fd524afec9068e891
cwhitti <112997645+cwhitti@users.noreply.github.com>
2024-02-24 22:59:09 -0700

    New bot

---
+++ new_bot.py
@@ -0,0 +1,13 @@
+import discord
+import subprocess
+import datetime
+import os
+import sys
+import time
+import stats
+from class_lookup import *
+from classes import name_list
+from datetime import date
+from discord.ext import commands
+from discord import app_commands
+import pandas as pd
