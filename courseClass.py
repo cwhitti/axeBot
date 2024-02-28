32b745f66198521db40ee99405d4c8f2f17d4b50
cwhitti <112997645+cwhitti@users.noreply.github.com>
2024-02-27 23:53:14 -0700

    Added files for grade lookup

--- courseClass.py
+++ courseClass.py
@@ -12,3 +12,32 @@ class Course:
         self.url = url
         self.prereqs = prereqs
         self.offered = offered
+        self.total = None
+        self.A = None
+        self.B = None
+        self.C = None
+        self.D = None
+        self.F = None
+        self.AU = None
+        self.P = None
+        self.NG = None
+        self.W = None
+        self.I = None
+        self.IP = None
+        self.Pen = None
+
+    def update_grades( self, A, B, C, D, F, AU, P, NG, W, I, IP, Pen, total ):
+
+        self.A = A
+        self.B = B
+        self.C = C
+        self.D = D
+        self.F = F
+        self.AU = AU
+        self.P = P
+        self.NG = NG
+        self.W = W
+        self.I = I
+        self.IP = IP
+        self.Pen = Pen
+        self.total = total
